de3fbce26e069ce72eb1fb43372d4a9de29bc87e
Garmhausen <robertgarmhausen@gmail.com>
2021-03-18 13:40:28 -0400

    a quick pass at drying off the code and improving readability, because I couldn't leave it alone and I had five minutes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ then pause for 3 minutes, and then continue voting for 30 seconds, pausing 3 min
 
 */
 
- chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 const driver = new Builder()
   .forBrowser('chrome')
   .build();
@@ -18,51 +18,60 @@ const driver = new Builder()
 let innerInterval;
 
 async function attemptClicks() {
-  let ben, voteButton, returnToPoll;
-
-  try {
-    ben = driver.findElement(By.xpath('//span[text()="Ben Gilbert, Broughton"]'));
-    voteButton = driver.findElement(By.xpath("//a[contains(concat(' ',normalize-space(@class),' '),' css-vote-button ')]//span"));
-    await (await driver).executeScript("arguments[0].scrollIntoView()", ben);
-    (await ben).click();
-    (await voteButton).click();
-  } catch {
-    returnToPoll = driver.findElement(By.xpath('//a[text()="Return To Poll"]'));
-    (await returnToPoll).click();
+  const clickBenAndClickVote = async () => {
+    const ben = await driver.findElement(By.xpath('//span[text()="Ben Gilbert, Broughton"]'));
+    const voteButton = await driver.findElement(By.xpath("//a[contains(concat(' ',normalize-space(@class),' '),' css-vote-button ')]//span"));
+    await driver.executeScript("arguments[0].scrollIntoView()", ben);
+    await ben.click();
+    await voteButton.click();
   }
 
-  await (await driver).sleep(500); // .5 seconds
+  const clickReturnToPoll = async () => {
+    const returnToPoll = await driver.findElement(By.xpath('//a[text()="Return To Poll"]'));
+    await returnToPoll.click();
+  }
+
+  // Sometimes the poll is slow to load and stutters.
+  // If there is an error finding an element, it will check for the older state,
+  // just kind of doing a flip/flop if needed.
 
   try {
-    returnToPoll = driver.findElement(By.xpath('//a[text()="Return To Poll"]'));
-    (await returnToPoll).click();
+    clickBenAndClickVote();
   } catch {
-    ben = driver.findElement(By.xpath('//span[text()="Ben Gilbert, Broughton"]'));
-    voteButton = driver.findElement(By.xpath("//a[contains(concat(' ',normalize-space(@class),' '),' css-vote-button ')]//span"));
-    await (await driver).executeScript("arguments[0].scrollIntoView()", ben);
-    (await ben).click();
-    (await voteButton).click();
+    clickReturnToPoll();
+  }
+
+  await driver.sleep(500); // .5 seconds
+
+  try {
+    clickReturnToPoll();
+  } catch {
+    clickBenAndClickVote();
   }
 }
 
 async function startInterval() {
-  console.log('beginning a round of voting.', DateTime.now().toLocaleString(DateTime.TIME_WITH_SECONDS));
+  console.log('beginning a round of voting.', getTime());
   innerInterval = setInterval(async () => {
     await attemptClicks();
   }, 2000); // 2 seconds
 }
 
 function stopInterval() {
-  console.log('waiting 3 minutes', DateTime.now().toLocaleString(DateTime.TIME_WITH_SECONDS));
+  console.log('waiting 3 minutes', getTime());
   clearInterval(innerInterval);
 }
 
 function startBot() {
-  console.log('starting bot in 3 minutes...', DateTime.now().toLocaleString(DateTime.TIME_WITH_SECONDS));
+  console.log('starting bot in 3 minutes...', getTime());
   setInterval(() => {
     startInterval();
     setTimeout(stopInterval, 30000); // 30 seconds
   }, 180000); // 3 minutes
+}
+
+const getTime = () => {
+  return DateTime.now().toLocaleString(DateTime.TIME_WITH_SECONDS)
 }
 
 const init = () => {
